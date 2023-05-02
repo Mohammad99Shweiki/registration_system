e8e9823057089d04d225ffdf8b96ee08b58020db
Mohammad99Shweiki <mohammadshwiki226@gmail.com>
2023-05-02 16:06:27 +0300

    simple refactor

--- models/db_students.py
+++ models/db_students.py
@@ -11,3 +11,30 @@ def get_student_by_id(student_id):
 def get_student_by_registration_key(registration_key):
     student = db(db.students.registration_key == registration_key).select().first()
     return student
+
+def save_student(first_name, last_name, email, password, registration_key, reset_password_key, registraion_id):
+    new_student = db.students.insert(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        registration_key=registration_key,
+        reset_password_key=reset_password_key,
+        registraion_id=registraion_id
+    )
+    
+    return new_student
+
+
+def edit_student(student, first_name, last_name, email, password, registration_key):
+    if first_name:
+        student.update_record(first_name=first_name)
+    if last_name:
+        student.update_record(last_name=last_name)
+    if email:
+        student.update_record(email=email)
+    if password:
+        student.update_record(password=password)
+    if registration_key:
+        student.update_record(registration_key=registration_key)
+
